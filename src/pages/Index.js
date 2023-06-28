afe43b6d868f97b92085ecee5f46a75b20f71957
Rowth <support@rowthtech.com>
2023-06-28 09:59:48 +0530

    first commit

---
+++ src/pages/Index.js
@@ -0,0 +1,16 @@
+import MainHeader from "./mainHeader/MainHeader"
+import Header from "./header/Header"
+import Footer from "./footer/Footer"
+
+const Index = () => {
+    return (
+        <div>
+            <MainHeader />
+            <Header />
+            Index
+            <Footer />
+        </div>
+    )
+}
+
+export default Index
---
+++ src/pages/header/header.js
@@ -0,0 +1,10 @@
+
+const header = () => {
+    return (
+        <div>
+            header
+        </div>
+    )
+}
+
+export default header
